4d0470f810209b93c93bb4c78ea08b2796a6a6fd
Jonathan <jonathan.prieto@me.com>
2022-11-27 15:33:37 -0500

    Add ID Ciudad to parsed data.

--- TSP/parser.cpp
+++ TSP/parser.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 
 void llenar_datos(ifstream& ff){
-    vector<tuple<int, int>> coordenas;
+    vector<tuple<int, int, int>> coordenas;
     int numero_puntos;
     int coordenadaX;
     int coordenadaY;
@@ -20,11 +20,11 @@ void llenar_datos(ifstream& ff){
         ff >> idCiudad;
         ff >> coordenadaX;
         ff >> coordenadaY;
-        coordenas.emplace_back(coordenadaX, coordenadaY);
+        coordenas.emplace_back(idCiudad, coordenadaX, coordenadaY);
     }
 
     for (int i = 0; i<coordenas.size(); i++){
-        cout << get<0>(coordenas[i]) << ", " << get<1>(coordenas[i]) << endl;
+        cout << get<0>(coordenas[i]) << ", " << get<1>(coordenas[i]) << ", " << get<2>(coordenas[i]) << endl;
     }
 }
 
